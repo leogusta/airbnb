c18f2e2daae5dfe789ac0119f978b54b62c9381e
leogusta <leogusta91@gmail.com>
2024-07-16 15:56:29 +0700

    feat: update profile image action supabase

--- utils/actions.ts
+++ utils/actions.ts
@@ -5,6 +5,7 @@ import db from "./db";
 import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { uploadImage } from "./supabase";
 
 const getAuthUser = async () => {
   const user = await currentUser();
@@ -111,7 +112,17 @@ export const updateProfileImageAction = async (
   try {
     const image = formData.get("image") as File;
     const validatedFields = validateWithZodSchema(imageSchema, { image });
+    const fullPath = await uploadImage(validatedFields.image);
 
+    await db.profile.update({
+      where: {
+        clerkId: user.id,
+      },
+      data: {
+        profileImage: fullPath,
+      },
+    });
+    revalidatePath("/profile");
     return { message: "Profile image updated successfully" };
   } catch (error) {
     return renderError(error);

6bb295d8250589f7adb182568ab67143c1546957
leogusta <leogusta91@gmail.com>
2024-07-18 14:34:34 +0700

    feat: Price Input

---
+++ components/form/PriceInput.tsx
@@ -0,0 +1,30 @@
+import { Label } from "@/components/ui/label";
+import { Input } from "../ui/input";
+import { Prisma } from "@prisma/client";
+
+// const name = Prisma.PropertyScalarFieldEnum.price;
+
+type FormInputNumberProps = {
+  defaultValue?: number;
+};
+
+function PriceInput({ defaultValue }: FormInputNumberProps) {
+  const name = "price";
+  return (
+    <div className="mb-2">
+      <Label htmlFor="price" className="capitalize">
+        Price ($)
+      </Label>
+      <Input
+        id={name}
+        type="number"
+        name={name}
+        min={0}
+        defaultValue={defaultValue || 100}
+        required
+      />
+    </div>
+  );
+}
+
+export default PriceInput;
